080b3b8e0f6bff92a0b9dd87dd790e053ecffbca
Max Bernauer <max_bernauer@gmx.net>
2022-08-29 17:31:29 +0200

    some useless tries to stream tweets

---
+++ tweet_test_app/StreamingTweetData.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.functions import col, split
+from pyspark.sql.types import *
+
+if __name__ == "__main__":
+
+    # create Spark session
+    spark = SparkSession.builder.appName(
+        "TwitterSentimentAnalysis").getOrCreate()
+
+    # read the tweet data from socket
+    tweet_df = spark \
+        .readStream \
+        .format("socket") \
+        .option("host", "127.0.0.1") \
+        .option("port", 3333) \
+        .load()
+
+    # type cast the column value
+    tweet_df_string = tweet_df.selectExpr("CAST(value AS STRING)")
+
+    print(tweet_df_string)
+
+    # split words based on space, filter out hashtag values and group them up
+    tweets_tab = tweet_df_string.withColumn('word', explode(split(col('value'), ' '))) \
+        .groupBy('word') \
+        .count() \
+        .sort('count', ascending=False). \
+        filter(col('word').contains('#'))
+
+    print(tweets_tab)
+
+    # write the above data into memory. consider the entire analysis in all iteration (output mode = complete). and let the trigger runs in every 2 secs.
+    writeTweet = tweets_tab.writeStream. \
+        outputMode("complete"). \
+        format("memory"). \
+        queryName("tweetquery"). \
+        trigger(processingTime='2 seconds'). \
+        start()
+
+    print(writeTweet)
+
+    print("----- streaming is running -------")
