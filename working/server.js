643230d1b1a2c69a670df1a7244fc7e420275b7c
annika2407 <annika.dackermann@gmx.de>
2022-08-30 15:08:39 +0200

    changes to server.js & database calls

--- working/server.js
+++ working/server.js
@@ -63,6 +63,7 @@ async function getFromDatabase(userid) {
 		console.log("Executing query " + query)
 		let res = await connection.query(query, [userid])
 		let row = res[0]
+		console.log(res)
 
 		if (row) {
 			console.log("Query result = ", row)
@@ -70,40 +71,186 @@ async function getFromDatabase(userid) {
 		} else {
 			return null;
 		}
-
+data
 	} finally {
 		if (connection)
 			connection.end()
 	}
 }
 
+
+
+
+//Get data from database
+async function getFromDatabaseStartpage() {
+	let connection
+	let query = 'SELECT * from popular_genres LIMIT 10 DESC'; // sorted by & desc !!!
+
+	try {
+		connection = await pool.getConnection()
+		console.log("Executing query " + query)
+		let res = await connection.query(query, [])
+		// let row = res[0]
+		console.log(res)
+
+		if (res) {
+			console.log("Query result = ", res)
+			// return row["genre"];
+			return res
+		} else {
+			return null;
+		}
+data
+	} finally {
+		if (connection)
+			connection.end()
+	}
+}
+
+
+
+
+
+
+
+
+
+async function getFromDatabase2(userid) {
+	let connection
+	let query = 'SELECT * from genres where genre=${userid}';
+
+	try {
+		connection = await pool.getConnection()
+		console.log("Executing query " + query)
+		let res = await connection.query(query, [userid])
+		let row = res[0]
+		console.log(res)
+
+		if (row) {
+			console.log("Query result = ", row)
+			return row["description"];
+		} else {
+			return null;
+		}
+data
+	} finally {
+		if (connection)
+			connection.end()
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
 //Send HTML response to client
 function send_response(response, data) {
-	response.send(`<h1>Hello k8s</h1>
+	response.send(`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Music</title>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
+			<script>
+				function fetchRandomMissions() {
+					const maxRepetitions = Math.floor(Math.random() * 200)
+					document.getElementById("out").innerText = "Fetching " + maxRepetitions + " random missions, see console output"
+					for(var i = 0; i < maxRepetitions; ++i) {
+						const missionId = Math.floor(Math.random() * 10)
+						console.log("Fetching mission id " + missionId)
+						fetch("/missions/sts-" + missionId, {cache: 'no-cache'})
+					}
+				}
+			</script>
+		</head>
+		<body>
+			<h1>All genre</h1>
+			<p>
+				<a href="javascript: fetchRandomMissions();">Randomly fetch some missions</a>
+				<span id="out"></span>
+			</p>
+<hr>
+			<h2>Information about the generated page</h4>
+	<h1>Hello k8s</h1>
 			<ul>
 				<li>Host ${os.hostname()}</li>
 				<li>Date: ${new Date()}</li>
 				<li>Memcached Servers: ${memcachedServers}</li>
 				<li>Result: <b>${data}</b></li>
-			</ul>`);
+			</ul>
+			</body>
+	</html>`);
 }
+
+
+function send_response_genre(response, data) {
+	response.send(`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Music</title>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
+			<script>
+				function fetchRandomMissions() {
+					const maxRepetitions = Math.floor(Math.random() * 200)
+					document.getElementById("out").innerText = "Fetching " + maxRepetitions + " random missions, see console output"
+					for(var i = 0; i < maxRepetitions; ++i) {
+						const missionId = Math.floor(Math.random() * 10)
+						console.log("Fetching mission id " + missionId)
+						fetch("/missions/sts-" + missionId, {cache: 'no-cache'})
+					}
+				}
+			</script>
+		</head>
+		<body>
+			<h1>All genre</h1>
+			<p>
+				<a href="javascript: fetchRandomMissions();">Randomly fetch some missions</a>
+				<span id="out"></span>
+			</p>
+<hr>
+			<h2>Information about the generated page</h4>
+	<h1>Hello k8s</h1>
+			<ul>
+				<li>Host ${os.hostname()}</li>
+				<li>Date: ${new Date()}</li>
+				<li>Memcached Servers: ${memcachedServers}</li>
+				<li>Result: <b>${data}</b></li>
+			</ul>
+			</body>
+	</html>`);
+}
+
+
+
 
 // Redirect / to person with ID l.mlb.com - p.7491
 app.get('/', function (request, response) {
-	response.writeHead(302, { 'Location': 'person/1456266659669282832' })
+	//response.writeHead(302, { 'Location': 'genre/pop' })
+// datenbank - 10 beliebsten
+	let data= await getFromDatabaseStartpage()
+	send_response(response,data)
 	response.end();
 })
 
 // Get data about a single person
-app.getAsync('/person/:id', async function (request, response) {
+// app.getAsync('/genre/:id', async function (request, response) {
+app.get('/genre/:id', async function (request, response) {
 	let userid = request.params["id"]
 	let key =  userid
 	let cachedata = await getFromCache(key)
 
-
 	if (cachedata) {
 		console.log(`Cache hit for key=${key}, cachedata = ${cachedata}`)
-		send_response(response, cachedata + " (cache hit)");
+		send_response_genre(response, cachedata + " (cache hit)");
 	} else {
 		console.log(`Cache miss for key=${key}, querying database`)
 		let data= await getFromDatabase(userid)
@@ -112,12 +259,15 @@ app.getAsync('/person/:id', async function (request, response) {
 			//console.log(`Got tweet=${description}, storing in cache`)
 			if (memcached)
 				await memcached.set(key, data, 30 /* seconds */);
-			send_response(response, data + " (cache miss) ");
+			send_response_genre(response, data + " (cache miss) ");
 		} else {
-			send_response(response, "No data found");
+			send_response_genre(response, "No data found");
 		}
 	}
 })
+
+
+
 
 // Add stress test endpoint, cf. https://github.com/bermi/inefficient
 app.get('/stress', inefficient);
